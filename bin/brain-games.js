f75c2713e047b17ac3713bed49018e7162ed8cb5
Reymezis <qq1s@bk.ru>
2020-06-28 18:02:21 +0700

    add eslint

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,4 +1,6 @@
 #!/usr/bin/env node
-import greeting from '/home/atn/frontend-project-lvl1/src/cli.js'
+
+import greeting from '/home/atn/frontend-project-lvl1/src/cli.js';
+
 console.log('Welcome to the Brain Games!');
 greeting();
