51b91e5fd153dc40476a172e17fb276ebdd57404
Nikolay Anisimov <qq1s@bk.ru>
2020-07-14 20:07:51 +0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import readlineSync from 'readline-sync';
 
+console.log('Welcome to the Brain Games!');
 const roundsCount = 3;
 
 const runEngine = (description, getGameData) => {
-  console.log('Welcome to the Brain Games!');
   const userName = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${userName}!`);
   console.log(description);
